b75be66943e778e1b7dc9d774e31ac906fd9244b
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-03 18:46:44 +0600

    php variable fandamental

---
+++ variable.php
@@ -0,0 +1,21 @@
+<?php 
+// a variables starts with $ dollar sign , followed by the name
+$name = "Jagadish Chakma";
+echo $name;
+
+//a variable starts with a letter or underscore character
+$age = 23;
+$_study = "BBA";
+echo $_study;
+
+// a variable name can not starts with a number
+// $1name = "Namita Chakma";
+
+// variavle name only containe alpa-numeric character or underscore
+$father_name = "Bijoy Chandra Chakma";
+echo $father_name;
+
+// variable name are case-sensetive
+$age = 23;
+$Age = 23;
+echo $age;
